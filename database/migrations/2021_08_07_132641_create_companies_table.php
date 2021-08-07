cf5a60ee86e785eeb3bfb19fb12a552f1f553802
Adson Santos Cardoso <adson.cardoso@defensoria.ba.def.br>
2021-08-07 11:08:43 -0300

    create orm companies table

---
+++ database/migrations/2021_08_07_132641_create_companies_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('symbol')->nullable();
+            $table->string('companyName')->nullable();
+            $table->integer('employees')->nullable();
+            $table->string('exchange')->nullable();
+            $table->string('industry')->nullable();
+            $table->string('website')->nullable();
+            $table->text('description')->nullable();
+            $table->string('CEO')->nullable();
+            $table->string('securityName')->nullable();
+            $table->string('issueType')->nullable();
+            $table->string('sector')->nullable();
+            $table->integer('primarySicCode')->nullable();
+            $table->string('address')->nullable();
+            $table->string('address2')->nullable();
+            $table->string('state')->nullable();
+            $table->string('city')->nullable();
+            $table->integer('zip')->nullable();
+            $table->string('country')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies')->nullable();
+    }
+}
